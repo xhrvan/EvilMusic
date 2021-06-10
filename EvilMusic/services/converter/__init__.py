ba19cadf9a759dd5ad7133e9769ac8ef33149065
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-10 13:25:59 +0530

    Update __init__.py

--- EvilMusic/services/converter/__init__.py
+++ EvilMusic/services/converter/__init__.py
@@ -3,4 +3,4 @@ from os import listdir, mkdir
 if "raw_files" not in listdir():
     mkdir("raw_files")
 
-from ShasaMusic.services.converter.converter import convert
+from EvilMusic.services.converter.converter import convert
