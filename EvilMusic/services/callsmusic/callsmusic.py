cd8b1903cc372c0f81f9851b528e7671155c6b25
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-10 13:25:28 +0530

    Update callsmusic.py

--- EvilMusic/services/callsmusic/callsmusic.py
+++ EvilMusic/services/callsmusic/callsmusic.py
@@ -18,8 +18,8 @@
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 
-from ShasaMusic.config import API_HASH, API_ID, SESSION_NAME
-from ShasaMusic.services.callsmusic import queues
+from EvilMusic.config import API_HASH, API_ID, SESSION_NAME
+from EvilMusic.services.callsmusic import queues
 
 client = Client(SESSION_NAME, API_ID, API_HASH)
 pytgcalls = PyTgCalls(client)
