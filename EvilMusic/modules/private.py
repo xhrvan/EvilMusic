660009694605a9cb218677463b0898b051b3018c
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-10 13:50:06 +0530

    Update private.py

--- EvilMusic/modules/private.py
+++ EvilMusic/modules/private.py
@@ -1,4 +1,4 @@
-# Shasamusic (Telegram bot project )
+# Evilmusic (Telegram bot project )
 # Copyright (C) 2021  Inukaasith
 
 # This program is free software: you can redistribute it and/or modify
@@ -15,10 +15,10 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import logging
-from ShasaMusic.modules.msg import Messages as tr
+from EvilMusic.modules.msg import Messages as tr
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
-from ShasaMusic.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL,BOT_USERNAME
+from EvilMusic.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL,BOT_USERNAME
 logging.basicConfig(level=logging.INFO)
 
 @Client.on_message(filters.private & filters.incoming & filters.command(['vchelp']))
