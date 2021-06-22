0bea2f33129b9d6d3d5d9da3dd044b1580a1be72
SHRVAN <82793048+shrvan42@users.noreply.github.com>
2021-06-22 20:18:17 +0530

    Update msg.py

--- EvilMusic/modules/msg.py
+++ EvilMusic/modules/msg.py
@@ -1,4 +1,4 @@
-# Daisyxmusic (Telegram bot project )
+# EvilMusic (Telegram bot project )
 # Copyright (C) 2021  Inukaasith
 
 # This program is free software: you can redistribute it and/or modify
@@ -15,7 +15,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import os
-from DaisyXMusic.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL
+from EvilMusic.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL
 class Messages():
       START_MSG = "**Hello 👋 [{}](tg://user?id={})!**\n\n🤖 I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels.\n\n✅ Send me /help for more info."
       HELP_MSG = [
